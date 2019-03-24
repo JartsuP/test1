1a99c9a0545687d13d695bc6106785dc056d95b2
Jari Putaansuu <jariputaansuu@gmail.com>
2019-03-24 23:20:37 +0200

    First change

---
+++ test_php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Form</title>       
+    </head>
+    <style>
+    </style>
+    <body>
+        <?php
+            echo "Joopa joo jaapa jaa ja tralla tralla laa!";
+        ?>
+    </body>
+</html>
